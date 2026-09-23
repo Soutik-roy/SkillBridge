c348b021c64176e81a3c4ef0d107dfe6b2b39b2d
Soutik roy <soutikroy10@gmail.com>
2026-09-24 02:00:01 +0530

    fix: updated CORS to allow Vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["*"],  # Allows all domains (like Vercel) to access the API
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
